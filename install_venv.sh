a679e9f2d1399ab521de04d9e88d0c4f907be38c
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2025-03-16 00:03:07 +0100

    Update install script to use pyenv to get the correct python version

--- install_venv.sh
+++ install_venv.sh
@@ -1,13 +1,29 @@
-venv_path="ekorre-venv"
+set -euo pipefail
 
+venv_path="ekorre-venv"
+python_version="3.12.9"
+
+# Check if the virtual environment already exists
 if [ -d $venv_path ]; then
     echo "Virtual environment already exists at path '$venv_path'. Please remove it first if the intention is to recreate the venv."
     exit 1
 fi
 
-python3 -m venv $venv_path
+# Install Python version using pyenv if not already installed
+if ! pyenv versions | grep -q $python_version; then
+    echo "Installing Python $python_version using pyenv..."
+    pyenv install $python_version
+else
+    echo "Python $python_version is already installed."
+fi
+
+# Create the virtual environment
+$(pyenv root)/versions/$python_version/bin/python3 -m venv $venv_path
+
+# Activate the virtual environment
 . $venv_path/bin/activate
 
-python3 -m pip install -r requirements.txt
+# Install the required packages
+python -m pip install -r requirements.txt
 
-echo ✓ Done! Activate using '. $venv_path/bin/activate'
+echo "✓ Done! Activate using '. $venv_path/bin/activate'"
