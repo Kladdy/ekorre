8b7f6be06d96f2fdbd24d0ace3fc32b0e4e9e60e
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2025-04-10 20:08:37 +0200

    Add support for loading many reactors

--- src/models/reactor.py
+++ src/models/reactor.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from pathlib import Path
 
+from mashumaro.codecs.yaml import YAMLDecoder, yaml_decode, yaml_encode
 from mashumaro.mixins.yaml import DataClassYAMLMixin
 
 
@@ -8,3 +10,12 @@ class ReactorOperatingData(DataClassYAMLMixin):
     reactor_label: str
     reactor_name: str
     reactor_type: str
+
+    @classmethod
+    def load_many_from_file(cls, file_path: str | Path):
+        if isinstance(file_path, str):
+            file_path = Path(file_path)
+        return yaml_decode(
+            file_path.read_text(),
+            list[cls],
+        )
