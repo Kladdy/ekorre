fd0e40b2acea76712478133725d5069b301f050e
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2026-01-25 13:28:54 +0100

    Add favicon to NiceGUI app initialization

--- src/pages/__init__.py
+++ src/pages/__init__.py
@@ -1,7 +1,7 @@
 import os
 
-from nicegui import ui
+from nicegui import app, ui
 
 from . import index, lekstuga, reactor_operating_data
 
-ui.run(port=int(os.getenv("NICEGUI_PORT")), dark=True)
+ui.run(port=int(os.getenv("NICEGUI_PORT")), dark=True, favicon="🐿️")
